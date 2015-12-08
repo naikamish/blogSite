0fc3658a10793d11fe8aed2ce24727feab4db833
naikamish <naikamish0822@gmail.com>
2015-12-08 01:17:11 -0500

    Convert blog template to javascript

--- client/templates/dashboard/dashboard.js
+++ client/templates/dashboard/dashboard.js
@@ -1,6 +1,5 @@
 Template.dashboard.onRendered( function () {
 	Session.set('dashboardTemplate','dashboardHome');
-	console.log(Session.get('dashboardTemplate'));
 }); 
 
 Template.dashboard.events({
@@ -8,13 +7,11 @@ Template.dashboard.events({
 		Session.set('dashboardTemplate', e.currentTarget.id);
 		$('.dashboardPill').removeClass("active");
         $(e.currentTarget).addClass("active");
-        console.log(Session.get('dashboardTemplate'));
 	}
 });
 
 Template.dashboard.helpers({
 	dashboardPill: function(templateName){
-		console.log(Session.equals('dashboardTemplate', templateName));
     	return Session.equals('dashboardTemplate', templateName);
     }
 });
--- client/templates/index/userPosts.js
+++ client/templates/index/userPosts.js
@@ -1,8 +1,20 @@
+Template.userPosts.onRendered( function () {
+    let template = '<div class="container-fluid container-userPosts"><div class="jumbotron"><h1>My Blog</h1></div>'+
+                        '{{#each getPosts}}<div class="row"><div class="col-xs-10 col-xs-offset-1 postDiv">'+
+                        '<a href="/{{userID}}/{{_id}}"><h3>{{postTitle}}</h3></a><br>{{postContent}}<br><br>'+
+                        '<span class="post-footer">Posted on {{postTimestamp}}</span><br><br></div></div>'+
+                        '<br><br><br>{{/each}}</div>';
+    let eachLoop = template.substring(template.search("{{#each getPosts}}")+18, template.search("{{/each}}"));
+    console.log(Template.currentData());
+    Session.set('blogTemplate',eachLoop);
+    //console.log(Session.get('blogTemplate'));
+}); 
+
 Template.userPosts.helpers({
 	getPosts: function(){
-		return this;
+        return Template.currentData();
     },
-    headerTemplate: function(){
+/*    headerTemplate: function(){
 		return BlogTemplate.findOne().header;
     },
     postStartTemplate: function(){
@@ -22,5 +34,5 @@ Template.userPosts.helpers({
     },
     footerTemplate: function(){
 		return BlogTemplate.findOne().footer;
-    }    
+    }    */
 });
