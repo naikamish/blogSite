772ede3910d150df5f6e02c0edddb53a05103e9b
naikamish <naikamish0822@gmail.com>
2015-12-10 22:31:47 -0500

    Fix minor issues with reactive page rendering

--- client/templates/index/addComment.js
+++ client/templates/index/addComment.js
@@ -1,34 +1,30 @@
 Template.addComment.onRendered( function () {
-    templateObject = BlogTemplate.findOne({'userID':Template.currentData().userID});
-    template = templateObject.cssTemplate+templateObject.individualPostTemplate;
 
-    template = template.replace("{{postTitle}}", Template.currentData().postTitle);
-    template = template.replace("{{postContent}}", Template.currentData().postContent);
-
-    let postComments = BlogComments.find({'postID': Template.currentData()._id}).fetch();
-
-    let eachLoop = template.substring(template.search("{{#each postComments}}")+22, template.search("{{/each}}"));
-    let fullEachLoop = "";
-    $.each(postComments, function( index, value ) {
-        let tempEachLoop = eachLoop;
-        tempEachLoop = tempEachLoop.replace("{{commentUserID}}", value.commentUserID);
-        tempEachLoop = tempEachLoop.replace("{{commentContent}}", value.commentContent);
-        tempEachLoop = tempEachLoop.replace("{{commentTimestamp}}", value.commentTimestamp);
-        fullEachLoop += tempEachLoop;
-    });
-    let templateStart = template.substring(0, template.search("{{#each postComments}}"));
-    let templateEnd = template.substring(template.search("{{/each}}")+9);
-    let fullTemplate = templateStart+fullEachLoop+templateEnd;
-    Session.set('individualPostTemplate',fullTemplate);
-    //console.log(templateStart);
-    console.log(fullTemplate);
-    //console.log(templateEnd);
-    //console.log(Session.get('blogTemplate'));
 });
 
 Template.addComment.helpers({
 	postComments: function(){
-		return Session.get('individualPostTemplate');
+	    templateObject = BlogTemplate.findOne({'userID':Template.currentData().userID});
+	    template = templateObject.cssTemplate+templateObject.individualPostTemplate;
+
+	    template = template.replace("{{postTitle}}", Template.currentData().postTitle);
+	    template = template.replace("{{postContent}}", Template.currentData().postContent);
+
+	    let postComments = BlogComments.find({'postID': Template.currentData()._id}).fetch();
+
+	    let eachLoop = template.substring(template.search("{{#each postComments}}")+22, template.search("{{/each}}"));
+	    let fullEachLoop = "";
+	    $.each(postComments, function( index, value ) {
+	        let tempEachLoop = eachLoop;
+	        tempEachLoop = tempEachLoop.replace("{{commentUserID}}", value.commentUserID);
+	        tempEachLoop = tempEachLoop.replace("{{commentContent}}", value.commentContent);
+	        tempEachLoop = tempEachLoop.replace("{{commentTimestamp}}", value.commentTimestamp);
+	        fullEachLoop += tempEachLoop;
+	    });
+	    let templateStart = template.substring(0, template.search("{{#each postComments}}"));
+	    let templateEnd = template.substring(template.search("{{/each}}")+9);
+	    let fullTemplate = templateStart+fullEachLoop+templateEnd;
+		return fullTemplate;
 	}
 });
 
--- client/templates/index/userPosts.js
+++ client/templates/index/userPosts.js
@@ -1,34 +1,30 @@
 Template.userPosts.onRendered( function () {
-    templateObject = BlogTemplate.findOne({'userID':Template.currentData()});
-    template = templateObject.cssTemplate+templateObject.postsTemplate;
-    let blogUser = Template.currentData();
-    let blogPosts = BlogPosts.find({'userID':blogUser}).fetch();
-    let blogTitle = Meteor.users.findOne({'_id':blogUser}).profile.blogTitle;
-    template = template.replace("{{blogTitle}}", blogTitle);
 
-    let eachLoop = template.substring(template.search("{{#each getPosts}}")+18, template.search("{{/each}}"));
-    let fullEachLoop = "";
-    $.each(blogPosts, function( index, value ) {
-        let tempEachLoop = eachLoop;
-        tempEachLoop = tempEachLoop.replace("{{userID}}", value.userID);
-        tempEachLoop = tempEachLoop.replace("{{_id}}", value._id);
-        tempEachLoop = tempEachLoop.replace("{{postTitle}}", value.postTitle);
-        tempEachLoop = tempEachLoop.replace("{{postContent}}", value.postContent);
-        tempEachLoop = tempEachLoop.replace("{{postTimestamp}}", value.postTimestamp);
-        fullEachLoop += tempEachLoop;
-    });
-    let templateStart = template.substring(0, template.search("{{#each getPosts}}"));
-    let templateEnd = template.substring(template.search("{{/each}}")+9);
-    let fullTemplate = templateStart+fullEachLoop+templateEnd;
-    Session.set('blogTemplate',fullTemplate);
-    //console.log(templateStart);
-    console.log(fullTemplate);
-    //console.log(templateEnd);
-    console.log(Session.get('blogTemplate'));
 }); 
 
 Template.userPosts.helpers({
 	getPosts: function(){
-        return Session.get('blogTemplate');
+        templateObject = BlogTemplate.findOne({'userID':Template.currentData()});
+        template = templateObject.cssTemplate+templateObject.postsTemplate;
+        let blogUser = Template.currentData();
+        let blogPosts = BlogPosts.find({'userID':blogUser}).fetch();
+        let blogTitle = Meteor.users.findOne({'_id':blogUser}).profile.blogTitle;
+        template = template.replace("{{blogTitle}}", blogTitle);
+
+        let eachLoop = template.substring(template.search("{{#each getPosts}}")+18, template.search("{{/each}}"));
+        let fullEachLoop = "";
+        $.each(blogPosts, function( index, value ) {
+            let tempEachLoop = eachLoop;
+            tempEachLoop = tempEachLoop.replace("{{userID}}", value.userID);
+            tempEachLoop = tempEachLoop.replace("{{_id}}", value._id);
+            tempEachLoop = tempEachLoop.replace("{{postTitle}}", value.postTitle);
+            tempEachLoop = tempEachLoop.replace("{{postContent}}", value.postContent);
+            tempEachLoop = tempEachLoop.replace("{{postTimestamp}}", value.postTimestamp);
+            fullEachLoop += tempEachLoop;
+        });
+        let templateStart = template.substring(0, template.search("{{#each getPosts}}"));
+        let templateEnd = template.substring(template.search("{{/each}}")+9);
+        let fullTemplate = templateStart+fullEachLoop+templateEnd;
+        return fullTemplate;
     }
 });
