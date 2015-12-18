d732b1bb8fdf29e7e0261e7192ee1bbd154f3b4f
naikamish <naikamish0822@gmail.com>
2015-12-18 02:11:40 -0500

    Add page navigation for blog posts and add default blog post on account creation

--- client/templates/dashboard/editTemplate.js
+++ client/templates/dashboard/editTemplate.js
@@ -1,4 +1,5 @@
 Template.editTemplate.onRendered(function () {
+    Session.set('blogPostsPage',1);
 	var postsEditor = ace.edit("postsEditor");
     postsEditor.setTheme("ace/theme/twilight");
     postsEditor.getSession().setMode("ace/mode/html");
@@ -35,10 +36,13 @@ Template.editTemplate.helpers({
         template = Session.get('cssTemplate')+Session.get('postsTemplate');
         let blogUser = Meteor.userId();
         let blogPosts = BlogPosts.find({'userID':blogUser}, {sort: {postTimestamp: -1}}).fetch();
+        let numberOfPages = blogPosts.length;
         let blogTitle = Meteor.users.findOne({'_id':blogUser}).profile.blogTitle;
         template = template.replace("{{blogTitle}}", blogTitle);
-        let eachLoop = template.substring(template.search("{{#each getPosts}}")+18, template.search("{{/each}}"));
+        let eachLoop = template.substring(template.search("{{#each BlogPost}}")+18, template.search("{{/each BlogPost}}"));
         let fullEachLoop = "";
+        let page = Session.get('blogPostsPage');
+        blogPosts = blogPosts.slice(page-1, page);
         $.each(blogPosts, function( index, value ) {
             let tempEachLoop = eachLoop;
             tempEachLoop = tempEachLoop.replace("{{userID}}", value.userID);
@@ -49,9 +53,35 @@ Template.editTemplate.helpers({
             tempEachLoop = tempEachLoop.replace("{{postTimestamp}}", formattedDate);
             fullEachLoop += tempEachLoop;
         });
-        let templateStart = template.substring(0, template.search("{{#each getPosts}}"));
-        let templateEnd = template.substring(template.search("{{/each}}")+9);
+        let templateStart = template.substring(0, template.search("{{#each BlogPost}}"));
+        let templateEnd = template.substring(template.search("{{/each BlogPost}}")+18);
         let fullTemplate = templateStart+fullEachLoop+templateEnd;
+
+        let pagesLoop = template.substring(template.search("{{#each BlogPage}}")+18, template.search("{{/each BlogPage}}"));
+        let fullPagesLoop = "";
+        fullPagesLoop += numberOfPages>0 ? pagesLoop.replace("{{page#}}",1):"";
+        fullPagesLoop += numberOfPages>1 ? pagesLoop.replace("{{page#}}",2):"";
+        fullPagesLoop += numberOfPages>2&&(page<5||numberOfPages<8) ? pagesLoop.replace("{{page#}}",3):"";
+        fullPagesLoop += numberOfPages>3&&(page<5||numberOfPages<8) ? pagesLoop.replace("{{page#}}",4):"";
+        fullPagesLoop += numberOfPages>4&&(page<5||numberOfPages<8) ? pagesLoop.replace("{{page#}}",5):"";
+        fullPagesLoop += numberOfPages>5&&numberOfPages<8 ? pagesLoop.replace("{{page#}}",6):"";
+        fullPagesLoop += numberOfPages>6&&numberOfPages<8 ? pagesLoop.replace("{{page#}}",7):"";
+        fullPagesLoop += numberOfPages>7&&page>4 ? "...":"";
+
+        fullPagesLoop += page>4&&page<numberOfPages-3 ? pagesLoop.replace("{{page#}}",page-1):"";
+        fullPagesLoop += page>4&&page<numberOfPages-3 ? pagesLoop.replace("{{page#}}",page):"";
+        fullPagesLoop += page>4&&page<numberOfPages-3 ? pagesLoop.replace("{{page#}}",page+1):"";
+
+        fullPagesLoop += numberOfPages>7&&page<numberOfPages-3 ? "...":"";
+        fullPagesLoop += numberOfPages>7&&page>numberOfPages-4 ? pagesLoop.replace("{{page#}}",numberOfPages-4):"";
+        fullPagesLoop += numberOfPages>7&&page>numberOfPages-4 ? pagesLoop.replace("{{page#}}",numberOfPages-3):"";
+        fullPagesLoop += numberOfPages>7&&page>numberOfPages-4 ? pagesLoop.replace("{{page#}}",numberOfPages-2):"";
+        fullPagesLoop += numberOfPages>7 ? pagesLoop.replace("{{page#}}",numberOfPages-1):"";
+        fullPagesLoop += numberOfPages>7 ? pagesLoop.replace("{{page#}}",numberOfPages):"";
+        fullPagesLoopActive = fullPagesLoop.substring(0,fullPagesLoop.search(page)-1)+" class='active'"+fullPagesLoop.substring(fullPagesLoop.search(page)-1);
+        templateStart = fullTemplate.substring(0, fullTemplate.search("{{#each BlogPage}}"));
+        templateEnd = fullTemplate.substring(fullTemplate.search("{{/each BlogPage}}")+18);
+        fullTemplate = templateStart+fullPagesLoopActive+templateEnd;
         return fullTemplate;
     }
 });
--- server/server.js
+++ server/server.js
@@ -44,10 +44,24 @@ Meteor.startup(function () {
     },
 
     setDefaultTemplate: function(userID){
-      let postsTemplate = "<div class=\"container-fluid\" id=\"container-userPosts\">\n    <div class=\"jumbotron\">\n        <h1 id='blogTitle'>{{blogTitle}}</h1>\n    </div>\n    {{#each getPosts}}\n    <div class=\"row\">\n        <div class=\"col-xs-10 col-xs-offset-1 postDiv\">\n            <a href=\"/{{userID}}/{{_id}}\">\n                <h3>{{postTitle}}</h3>\n            </a>\n            <br>{{postContent}}<br><br>\n            <span class=\"post-footer\">{{postTimestamp}}</span>\n            <br><br>\n        </div>\n    </div>\n    <br><br><br>\n    {{/each}}\n</div>"
+      let postsTemplate = "<div class=\"container-fluid\" id=\"container-userPosts\">\n    <div class=\"jumbotron\">\n        <h1 id='blogTitle'>{{blogTitle}}</h1>\n    </div>\n    {{#each BlogPost}}\n    <div class=\"row\">\n        <div class=\"col-xs-10 col-xs-offset-1 postDiv\">\n            <a href=\"/{{userID}}/{{_id}}\">\n                <h3>{{postTitle}}</h3>\n            </a>\n            <br>{{postContent}}<br><br>\n            <span class=\"post-footer\">Posted on {{postTimestamp}}</span>\n            <br><br>\n        </div>\n    </div>\n    <br>\n    {{/each BlogPost}}\n    <ul id=\"pageNavigation\">\n        {{#each BlogPage}}\n        <a href=\"#\"><li>{{page#}}</li></a>\n        {{/each BlogPage}}\n    </ul>\n</div>";
       let individualPostTemplate = "<div class=\"container-fluid\" id=\"container-userPosts\">\n    <div class=\"row\">\n        <div class=\"col-xs-10 col-xs-offset-1 postDiv\">\n            <h3 id=\"postTitle\">{{postTitle}}</h3>\n            <br>\n            {{postContent}}\n        </div>\n    </div>\n    <br><br><br>\n    <div class=\"row\">\n        <div class=\"col-xs-10 col-xs-offset-1 postDiv\">\n            <h2>Comments</h2>\n            {{#each postComments}}\n                <span class=\"bold-span\">{{commentUserID}}</span>\n                <br>\n                {{commentContent}}\n                <br>\n                <span class=\"comment-footer\">{{commentTimestamp}}</span>\n                <br><br><br>\n            {{/each}}\n            <h4>Add Comment</h4>\n            <form>\n                <textarea id=\"postComment\"></textarea>\n                <br>\n                <input type=\"submit\" value=\"Submit\">\n            </form>\n        </div>\n    </div>\n</div>";
-      let cssTemplate = "<!--enter css code here-->\n<style>\n#container-userPosts {\n    background: url(\"/images/background.png\") fixed no-repeat 100% 100% !important;\n    min-height: 100vh;\n}\n</style>";
+      let cssTemplate = "<!--enter css code here-->\n<style>\n#container-userPosts {\n    background: url(\"/images/background.png\") fixed no-repeat 100% 100% !important;\n    min-height: 100vh;\n}\n\n#pageNavigation{\n    text-align:center;\n}\n#pageNavigation li\n{\n    display:inline-block;\n    list-style:none;\n    width:40px;\n    height:40px;\n    background-color:#000;\n    text-align:center;\n    color:#fff;\n    line-height:40px;\n    border-radius:50%;\n}\n#pageNavigation li:hover{\n    background-color:#007EB2;\n}\n#pageNavigation a{\n    text-decoration:none;\n    color:inherit;\n}\n\n#pageNavigation .active{\n    background-color:#007EB2;\n}\n</style>";
       BlogTemplate.insert({'userID':userID, 'postsTemplate':postsTemplate, 'individualPostTemplate':individualPostTemplate, 'cssTemplate':cssTemplate});
+    },
+
+    createSamplePost: function(userID){
+      let postTitle = "My First Blog Post"
+      let postContent = "This is your first blog post!"
+      let postTimestamp = new Date();
+      let blogPost = {
+        'userID': userID,
+        'postTitle': postTitle,
+        'postContent': postContent,
+        'postTimestamp':postTimestamp
+      };
+
+      BlogPosts.insert(blogPost);
     },
 
     updatePost: function(postId, postTitle, postContent){
@@ -56,21 +70,20 @@ Meteor.startup(function () {
   });
 });
 
-if (Meteor.isServer) {
-  Accounts.onCreateUser(function(options, user) {
-    let newEmail = user.emails[0].address
-    let validEmail = newEmail.match(/.+@.+\..+/i);
-    if(!validEmail){
-      throw new Meteor.Error(403, "Invalid Email");
+Accounts.onCreateUser(function(options, user) {
+  let newEmail = user.emails[0].address
+  let validEmail = newEmail.match(/.+@.+\..+/i);
+  if(!validEmail){
+    throw new Meteor.Error(403, "Invalid Email");
+  }
+  else{
+    Meteor.call('setDefaultTemplate', user._id);
+    Meteor.call('createSamplePost', user._id);
+    if(!options.profile){
+      options.profile = {'firstName':"Anonymous", 'blogTitle':"My First Blog Site"};
     }
-    else{
-      Meteor.call('setDefaultTemplate', user._id);
-      if(!options.profile){
-        options.profile = {'firstName':"Anonymous", 'blogTitle':"My First Blog Site"};
-      }
-      if (options.profile)
-        user.profile = options.profile;
-      return user;
-    }
-  });
-}
+    if (options.profile)
+      user.profile = options.profile;
+    return user;
+  }
+});
